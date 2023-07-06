617c370b9c566194325259c78d90c0d87bc83038
sahnoune2 <anankarta@gmail.com>
2023-07-06 15:22:15 +0100

    dist/main.js

--- src/place01.js
+++ src/place01.js
@@ -9,27 +9,30 @@ function placeO1(
   title1,
   cY
 ) {
+  //function to assign the colors //
+  function assign(text1, text2, text3, text4, text5, text6) {
+    first.style.backgroundColor = text1;
+    player.textContent = text2;
+    title2.style.backgroundColor = text3;
+    title1.style.backgroundColor = text4;
+    cX.clicked1 = text5;
+    cY.clicked2 = text6;
+  }
   if (cX.clicked1 === false) {
-    if (!first.hasChildNodes() && first.textContent !== null) {
-      first.textContent = "O";
-      player.textContent = "player2's turn";
-      title2.style.backgroundColor = "blue";
-      title1.style.backgroundColor = "";
-      cX.clicked1 = true;
-      cY.clicked2 = false;
-    } else if (
+    //when u dont guess the right spot//
+    if (!first.hasChildNodes() && first.style.backgroundColor !== "green") {
+      assign("green", "player2's turn", "blue", "", true, false);
+    }
+    //when u  guess the right spot//
+    else if (
       first.hasChildNodes() &&
-      first.textContent !== "O" &&
+      first.style.backgroundColor !== "green" &&
       first.style.backgroundColor !== "red"
     ) {
-      first.style.backgroundColor = "red";
-      player.textContent = "player2's turn";
-      title2.style.backgroundColor = "blue";
-      title1.style.backgroundColor = "";
-      cX.clicked1 = true;
-      cY.clicked2 = false;
+      assign("red", "player2's turn", "blue", "", true, false);
       objectX.countX += 1;
     }
+    //declaring the winner//
     if (objectX.countX === 9) {
       setTimeout(() => {
         alert("player1 wins");
--- src/place02.js
+++ src/place02.js
@@ -9,30 +9,31 @@ function placeO2(
   title1,
   cX
 ) {
+  //function to assign the colors //
+  function assign(text1, text2, text3, text4, text5, text6) {
+    second.style.backgroundColor = text1;
+    player.textContent = text2;
+    title2.style.backgroundColor = text3;
+    title1.style.backgroundColor = text4;
+    cX.clicked1 = text5;
+    cY.clicked2 = text6;
+  }
   let index = arrayB.indexOf(second);
   if (cY.clicked2 === false) {
-    if (!second.hasChildNodes() && second.textContent !== null) {
-      second.textContent = "O";
-      player.textContent = "player1's turn";
-      title1.style.backgroundColor = "yellow";
-      title2.style.backgroundColor = "";
-
-      cY.clicked2 = true;
-      cX.clicked1 = false;
-    } else if (
+    //when u dont guess the right spot//
+    if (!second.hasChildNodes() && second.style.backgroundColor !== "green") {
+      assign("green", "player1's turn", "yellow", "", false, true);
+    }
+    //when u  guess the right spot//
+    else if (
       second.hasChildNodes() &&
-      second.textContent !== "O" &&
+      second.style.backgroundColor !== "green" &&
       second.style.backgroundColor !== "blue"
     ) {
-      second.style.backgroundColor = "blue";
-
-      player.textContent = "player1's turn";
-      title1.style.backgroundColor = "yellow";
-      title2.style.backgroundColor = "";
-      cY.clicked2 = true;
-      cX.clicked1 = false;
+      assign("blue", "player1's turn", "yellow", "", false, true);
       objectY.countY += 1;
     }
+    //declaring the winner//
     if (objectY.countY === 9) {
       setTimeout(() => {
         alert("player2 wins");
